88151f106fd68b32b75ffeeb359d99f98d8e9a70
Juan Pablo Enriquez <laredlaboratorio@hotmail.com>
2018-10-03 18:15:04 -0500

    patron modulo

---
+++ js/app2.js
@@ -0,0 +1,30 @@
+var operandoa;
+var operandob;
+var operacion;
+var cantidadDecimal = false;
+var resultado = false;
+var signo = null;
+var reducir = 0;
+
+function calculadora() {
+	var display = document.getElementById("display");
+	var cero = document.getElementById("0");
+	var uno = document.getElementById("1");
+	var dos = document.getElementById("2");
+	var tres = document.getElementById("3");
+	var cuatro = document.getElementById("4");
+	var cinco = document.getElementById("5");
+	var seis = document.getElementById("6");
+	var siete = document.getElementById("7");
+	var ocho = document.getElementById("8");
+	var nueve = document.getElementById("9");
+	var mas = document.getElementById("mas");
+	var menos = document.getElementById("menos");
+	var por = document.getElementById("por");
+	var dividido = document.getElementById("dividido");
+	var igual = document.getElementById("igual");
+	var on = document.getElementById("on");
+	var punto = document.getElementById("punto");
+	var sign = document.getElementById("sign");
+	var raiz = document.getElementById("raiz");
+}
